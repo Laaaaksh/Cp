ef1af38d9712bec39f4ca7d8945c8a8a0f028881
Laksh Sadhwani <47391874+Laaaaksh@users.noreply.github.com>
2021-08-05 16:46:11 +0530

    added findminimumsortedarrat

--- errichto lecs/findminimuminrotatedsortedarray.cpp
+++ errichto lecs/findminimuminrotatedsortedarray.cpp
@@ -1,20 +1,8 @@
-class Solution {
-public:
-    int findMin(vector<int>& arr) {
-        int low = 0;
-        int n = arr.size();
-        int high = n - 1;
-        int ans = INT_MAX;
-        while(low <=high)
-        {
-            int mid = (low + high) >> 1;
-            if(arr[mid] >= arr[high]){
-                ans = mid;
-                low = mid + 1;
-            }
-            else 
-                high = mid;
-        }
-        return ans==INT_MAX ? arr[0] : arr[ans];
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main() {
+       int t ;
+       cin >> t;
+       cout << t;
+       return 0; 
     }
-};

595f794bdaa3785fa7dc0924868e5cb901407198
Laksh Sadhwani <47391874+Laaaaksh@users.noreply.github.com>
2021-08-23 14:29:47 +0530

    added sudoku solver

--- leetcode/sudokusolver.cpp
+++ leetcode/sudokusolver.cpp
@@ -1,46 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
-    }
-    // we need not return anything just need to fill the board
-    bool solve(vector<vector<char>>& board)
+    bool isValidSudoku(vector<vector<char> > &board)
     {
-        for(int i = 0 ; i < board.size(); i++)
-        {
-            for(int j = 0 ; j <board[0].size(); j++)
-            {
-                if(board[i][j]=='.')
+        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
+        
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != '.')
                 {
-                    for(char c = '1'; c <= '9' ; c++)
-                    {
-                        if(istrue(i,j,board,c))
-                        {
-                            board[i][j]=c;
-                            if(solve(board)==true)
-                                return true;
-                            else
-                                board[i][j]='.';
-                        }
-                    }// this is how you loop in the characters
-                    return false;
-                }// if loop ends
-            } // j for loop ends
-        }// first for loop ends
-        return true;
-    }
-    bool istrue(int row, int col, vector<vector<char>>& board, char c)
-    {
-        // we need to iterate in the entire board
-        for(int i = 0 ; i < 9 ; i ++)
-        {
-            if(board[row][i]==c)
-                return false;
-            if(board[i][col]==c)
-                return false;
-            if(board[3 * (row/3) + i / 3][ 3 *(col/3) + i % 3] == c)
-                return false;
-        }
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                        return false;
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                }
+        
         return true;
     }
 };

86a828a5ae34a452515c836c38bd42752c1026bc
Laksh Sadhwani <47391874+Laaaaksh@users.noreply.github.com>
2021-08-29 18:25:22 +0530

    empty

--- Coding_practice/file.cpp
+++ Coding_practice/file.cpp
@@ -31,12 +31,13 @@ template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
 
 const long long inf=1e18;
 const long long P2LM = 2e18;
-const int32_t mod=1e9+7;
+const int mod=1e9+7;
 const char n1 = '\n';
 const char space = ' ';
 const int32_t MM=998244353;
 const int N=50005;
 const int MX = 100001;
+const int maxl = 86028121;
 
 
 struct DSU {
@@ -63,29 +64,52 @@ int gcd(int a, int b) {
     while (b) b ^= a ^= b ^= a %= b;
     return a;
 }
-void solve(){ 
-    int n; cin>>n;
-    if(n<=1){
-     cout << "no" << n1;
-     return;
-    }
-    else{
-    for(int i=2 ;i*i<=n;i++)
+// void allprimes()
+// {
+//     rep(i,0,maxl)
+//         dp[i] = true;
+//     dp[0]=dp[1]=false;
+//     for(int i = 2 ; i*i<=maxl;i++)
+//     {
+//         if(dp[i]==1)
+//         {
+//             for(int j = i * i ; j<=maxl;j+=i)
+//             {
+//                 dp[j] = false;
+//             }
+//         }
+//     }
+//     rep(i,2,maxl)
+//     {
+//         if(dp[i]==true)
+//             result.pb(i);
+//     }
+
+// }
+void solve(){
+ int n; cin >> n;
+ string res = "";
+ while(n)
+ {
+    if(!(n&1))
     {
-        if(n%i==0){
-            cout << "no" << n1;
-            return;
-        }
+        res+='B';
+        n = n/2;
     }
+    else
+    {
+        res+='A';
+        n--;
     }
-   cout << "yes" << n1;
-   return;
+ }
+ reverse(res.begin(), res.end());
+ cout << res;
 }
 signed main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0); 
+    cin.tie(0);cout.tie(0);
     int t=1;
-    cin>>t;
+    // cin>>t;
     while(t--) solve();
     return 0;
 }
